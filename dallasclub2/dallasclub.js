71a21fa88d5951500ce7c2d097f4875b9acf0015
Vitorperez24 <vitordeoliveiraperez@gmail.com>
2024-05-11 20:37:45 -0300

    dallasclub

--- dallasclub2/dallasclub.js
+++ dallasclub2/dallasclub.js
@@ -1,9 +1,10 @@
+// Adicione isso ao seu JavaScript para controlar a exibição e ocultação da mensagem
+
 function ativarOferta() {
-    var mensagem = document.getElementById("mensagem");
-    mensagem.style.display = "block"; // Exibe a mensagem
-  
-    setTimeout(function(){ 
-      mensagem.style.display = "none"; // Esconde a mensagem após 2 segundos
-    }, 2000);
-  }
-  
+  var mensagem = document.getElementById("mensagem");
+  mensagem.classList.add("mostrar"); // Adiciona a classe "mostrar" para exibir a mensagem
+
+  setTimeout(function() {
+    mensagem.classList.remove("mostrar"); // Remove a classe "mostrar" após alguns segundos
+  }, 3000); // Tempo em milissegundos (3 segundos neste exemplo)
+}
