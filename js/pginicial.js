ce916d66e33c7a459f97e3f89d2d15983d996132
Vitorperez24 <vitordeoliveiraperez@gmail.com>
2024-05-13 21:07:04 -0300

    trabalhofinal

--- js/pginicial.js
+++ js/pginicial.js
@@ -5,3 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = "cadastro.html";
     });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    // Adicionando um ouvinte de evento ao botão
+    document.getElementById("btnentrar").addEventListener("click", function() {
+        // Redirecionando para a página de cadastro
+        window.location.href = "cadastro.html";
+    });
+});
