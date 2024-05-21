07ae1f46132c8e7cd8b57d3d3548c01a41f4ec27
Vitorperez24 <vitordeoliveiraperez@gmail.com>
2024-05-20 21:07:58 -0300

    pronto

--- js/dallasclub.js
+++ js/dallasclub.js
@@ -1,10 +1,7 @@
-// Adicione isso ao seu JavaScript para controlar a exibição e ocultação da mensagem
-
-function ativarOferta() {
-  var mensagem = document.getElementById("mensagem");
-  mensagem.classList.add("mostrar"); // Adiciona a classe "mostrar" para exibir a mensagem
-
-  setTimeout(function() {
-    mensagem.classList.remove("mostrar"); // Remove a classe "mostrar" após alguns segundos
-  }, 3000); // Tempo em milissegundos (3 segundos neste exemplo)
-}
+document.addEventListener("DOMContentLoaded", function() {
+  // Adicionando um ouvinte de evento ao botão
+  document.getElementById("dallas2").addEventListener("click", function() {
+      // Redirecionando para a página de cadastro
+      window.location.href = "dallasclub2.html";
+  });
+});
---
+++ js/telaprodutos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Adicionando um ouvinte de evento ao botão
+    document.getElementById("btnCadastrar").addEventListener("click", function() {
+        // Redirecionando para a página de cadastro
+        window.location.href = "cadastro.html";
+    });
+});
