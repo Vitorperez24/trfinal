e9c0ed8e19c0cc34b72eebb1314e3c1318928c4b
Vitorperez24 <vitordeoliveiraperez@gmail.com>
2024-05-24 20:01:38 -0300

    menu

--- js/login.js
+++ js/login.js
@@ -10,3 +10,10 @@ document.getElementById('forgot-password').addEventListener('click', function(ev
 document.getElementById('register-button').addEventListener('click', function() {
     window.location.href = 'register.html';
 });
+document.addEventListener("DOMContentLoaded", function() {
+    // Adicionando ouvinte de evento ao link "Esqueceu sua senha?"
+    document.getElementById("esqueceuSenhaLink").addEventListener("click", function(event) {
+        event.preventDefault(); // Prevenir o comportamento padrão do link
+        window.location.href = "recsenha.html"; // Redirecionar para a página de recuperação de senha
+    });
+});
